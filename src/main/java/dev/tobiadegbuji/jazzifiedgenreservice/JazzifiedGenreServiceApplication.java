6687baea7ebeb96ee3eb3e32ad392a0f5d6dd155
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-08-06 07:14:42 -0400

    first

---
+++ src/main/java/dev/tobiadegbuji/jazzifiedgenreservice/JazzifiedGenreServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.tobiadegbuji.jazzifiedgenreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazzifiedGenreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazzifiedGenreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tobiadegbuji/jazzifiedgenreservice/JazzifiedGenreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tobiadegbuji.jazzifiedgenreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazzifiedGenreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
